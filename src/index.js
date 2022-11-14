f778e8eadc6097ced233889befa1403c7d043d62
Das Tapan <tapands.das@gmail.com>
2022-11-14 20:58:53 +0530

    Add webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import './style.css';
 console.log('Jay Jagannath');
 console.log('Har Har Mahadev');
 console.log('Jay Bhabani');
+console.log('Jay Ganapati');
+console.log('Jay Surya Bhagban');
+
+console.log('Sri Ram Jay Ram ');
+
+let count = 101;
