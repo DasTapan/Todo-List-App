bf3482c2290403ff7c519ad5d22ede7af8e54d64
Das Tapan <tapands.das@gmail.com>
2022-11-16 11:58:10 +0530

    Set sidebar width

--- src/home.js
+++ src/home.js
@@ -13,8 +13,28 @@ const pageLoad = () => {
     footerElement.classList.add('footer');
     footerElement.textContent = 'DasTapan All Rights Reserved';
 
+    // add to DOM
     document.body.appendChild(contentDiv);
     contentDiv.append(headerElement, mainElement, footerElement);
+
+    const sidebar = document.createElement('div');
+    sidebar.classList.add('sidebar');
+
+    const midsection = document.createElement('div');
+    midsection.classList.add('midsection');
+
+    // add to DOM
+    mainElement.append(sidebar,midsection);
+
+    const projectHeading = document.createElement('h1');
+    projectHeading.classList.add('project-heading');
+    projectHeading.textContent = 'Default Project';
+
+    const taskContainer = document.createElement('div');
+    taskContainer.classList.add('task-container');
+
+    //add to DOM
+    midsection.append(projectHeading, taskContainer);
 };
 
 export default pageLoad;
